30e2b00821ab03a08daa9960f3e62935402518ef
gabriellapowell <soccerplayer60@icloud.com>
2018-06-06 22:08:12 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -1,31 +1,31 @@
 
-# def create_olympics_hash 
-# create_olympics_hash = {:Sydney => 2000, :Athens => 2004, :Beijing => 2008, :London => 2000}
-# end
+# # def create_olympics_hash 
+# # create_olympics_hash = {:Sydney => 2000, :Athens => 2004, :Beijing => 2008, :London => 2000}
+# # end
     
 
-# def add_a_key_value_pair
-# create_olympics_hash = {:Sydney => 2000, :Athens => 2004, :Beijing => 2008, :London => 2000}
-# new_city = :Atlanta
-# new_year = 1996
-# create_olympics_hash [new_city] = [new_year]
-# end
+# # def add_a_key_value_pair
+# # create_olympics_hash = {:Sydney => 2000, :Athens => 2004, :Beijing => 2008, :London => 2000}
+# # new_city = :Atlanta
+# # new_year = 1996
+# # create_olympics_hash [new_city] = [new_year]
+# # end
 
 
-# def iterate_through_hash
-#   summer_olympics = {:Sydney => 2000, :Athens => 2004, :Beijing => 2008, :London => 2000, :Atlanta => 1996}
-# place_array = []
+# # def iterate_through_hash
+# #   summer_olympics = {:Sydney => 2000, :Athens => 2004, :Beijing => 2008, :London => 2000, :Atlanta => 1996}
+# # place_array = []
 
-# puts "The #{year} summer olympics took place in #{place}."
-# end 
-# end
-# iterate_through_hash
-
-
+# # puts "The #{year} summer olympics took place in #{place}."
+# # end 
+# # end
+# # iterate_through_hash
 
 
-def iterate_through_keys
-summer_olympics = {:Sydney => 2000, :Athens => 2004, :Beijing => 2008, :London => 2000, :Atlanta => 1996}
-summer_olympics.key
 
-iterate_through_keys
+
+# def iterate_through_keys
+# summer_olympics = {:Sydney => 2000, :Athens => 2004, :Beijing => 2008, :London => 2000, :Atlanta => 1996}
+# summer_olympics.key
+
+# iterate_through_keys

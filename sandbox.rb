d83038183b4e475aaf9bfe871c56b21dbcc267ee
gabriellapowell <soccerplayer60@icloud.com>
2018-06-06 21:33:32 +0000

    automatically backed up by learn

--- sandbox.rb
+++ sandbox.rb
@@ -1,7 +1,7 @@
 def name_length(x)
  puts "What is your name?"
 x = gets.chomp.to_i 
-if name_length is =< 3
+if name_length is <= 3
   puts "your name is short"
   elsif name_length <= 7
   puts "you're average"
